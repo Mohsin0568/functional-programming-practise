e3548f55e7dea908a2dda253b3673945a5950159
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-02-15 15:29:32 +0530

    initial repository

---
+++ src/main/java/com/systa/practise/functional/programming/FuncationalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.systa.practise.functional.programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncationalProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncationalProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systa/practise/functional/programming/FuncationalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa.practise.functional.programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncationalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
